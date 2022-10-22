bb1c0309fbdb82f53417db183fa9c7916394f0aa
Mathias Buus <mathiasbuus@gmail.com>
2022-10-22 19:33:32 +0200

    fix keep alives for grpc

--- index.js
+++ index.js
@@ -38,7 +38,13 @@ function lightningRpc (opts = {}) {
 
   const params = {
     'grpc.max_receive_message_length': -1,
-    'grpc.max_send_message_length': -1
+    'grpc.max_send_message_length': -1,
+    'grpc.keepalive_time_ms': 10000,
+    'grpc.keepalive_timeout_ms': 5000,
+    'grpc.keepalive_permit_without_calls': true,
+    'grpc.http2.max_pings_without_data': 0,
+    'grpc.http2.min_time_between_pings_ms':10000,
+    'grpc.http2.min_ping_interval_without_data_ms': 5000
   }
 
   // pass the credentials when creating a channel
